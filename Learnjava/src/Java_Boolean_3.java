6f6f408e8f6ee184bf99a424647a433817793e50
paramasivan <paramasivan645@gmail.com>
2024-01-07 15:25:25 +0530

    Java Boolean

---
+++ Learnjava/src/Java_Boolean_3.java
@@ -0,0 +1,17 @@
+
+public class Java_Boolean_3 {
+
+	public static void main(String[] args) {
+		boolean isJavaFun = true;
+		boolean isFishTasty = false;
+		System.out.println(isJavaFun);     // Outputs true
+		System.out.println(isFishTasty);   // Outputs false
+		//A Boolean expression returns a boolean value: true or false
+		int x = 10;
+		int y = 9;
+		System.out.println(x > y); // returns true, because 10 is higher than 9
+		
+
+	}
+
+}
