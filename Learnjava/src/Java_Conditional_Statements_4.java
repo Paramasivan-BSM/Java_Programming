c1605ca1163ae7538c5a50c69be8030b908417f4
paramasivan <paramasivan645@gmail.com>
2024-01-07 15:52:17 +0530

    Java Conditional Statements

---
+++ Learnjava/src/Java_Conditional_Statements_4.java
@@ -0,0 +1,24 @@
+
+public class Java_Conditional_Statements_4 {
+
+	public static void main(String[] args) {
+		/*Less than: a < b
+Less than or equal to: a <= b
+Greater than: a > b
+Greater than or equal to: a >= b
+Equal to a == b
+Not Equal to: a != b
+You can use these conditions to perform different actions for different decisions.*/
+		
+		//1. if Statement
+		// Use the if statement to specify a block of Java code to be executed if a condition is true.
+		//Syntax
+		/*
+		 * if (condition) {
+  // block of code to be executed if the condition is true
+   * Note that if is in lowercase letters. Uppercase letters (If or IF) will generate an error.
+}*/
+
+	}
+
+}
