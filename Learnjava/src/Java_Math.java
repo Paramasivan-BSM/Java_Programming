c0eae4e6a09fbb85029619f22b10d209b277825e
paramasivan <paramasivan645@gmail.com>
2024-01-07 15:24:27 +0530

    Java Math

---
+++ Learnjava/src/Java_Math.java
@@ -0,0 +1,56 @@
+
+public class Java_Math {
+
+	public static void main(String[] args) {
+		//The Java Math class has many methods that allows you to perform mathematical tasks on numbers.
+		//!.Math.max(x,y)
+		//The Math.max(x,y) method can be used to find the highest value of x and y:
+		int A=10;
+		int B=20;
+		System.out.println(Math.max(A, B));
+		
+		//2.Math.min(x,y)
+		//The Math.min(x,y) method can be used to find the lowest value of x and y:
+		int A1=10;
+		int B1=20;
+		System.out.println(Math.min(A1, B1));
+		
+		//3.Math.sqrt(x)
+		//The Math.sqrt(x) method returns the square root of x:
+		int sqrt=100;
+		System.out.println(Math.sqrt(sqrt));
+		
+		//Math.abs(x)
+		//The Math.abs(x) method returns the absolute (positive) value of x:
+		int abs1=-20;
+		System.out.println(Math.abs(abs1));
+		
+		//Random Numbers
+		//Math.random() returns a random number between 0.0 (inclusive), and 1.0 (exclusive):
+		System.out.println(Math.random());
+		
+		//RJMM will adds in impending months
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
