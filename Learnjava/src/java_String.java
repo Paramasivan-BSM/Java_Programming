c0e228d68f6850b712bd58d3faa6f3e09b75acd1
paramasivan <paramasivan645@gmail.com>
2024-01-07 12:40:33 +0530

    .

--- Learnjava/src/java_String.java
+++ Learnjava/src/java_String.java
@@ -22,11 +22,23 @@ public class java_String {
 		System.out.println(txt.toLowerCase());   // Outputs "hello world"
 		
 		//Finding a Character in a String
-		String txt1 = "Please locate where 'locate' occurs!";
-		System.out.println(txt.indexOf("locate")); // Outputs 7 
+		String txt1 = "Hey Folks!";
+		System.out.println(txt1.indexOf("Folks!")); // Outputs 7 
 		//Java counts positions from zero.
 		//0 is the first position in a string, 1 is the second, 2 is the third ...
 		
+		//4.Java String Concatenation
+		//The + operator can be used between strings to combine them. This is called concatenation:
+		String firstName = "John";
+		String lastName = "Doe";
+		System.out.println(firstName + " " + lastName);
+		//Note that we have added an empty text (" ") to create a space between firstName and lastName on print.
+		//You can also use the concat() method to concatenate two strings:
+		String firstName1 = "John ";
+		String lastName1 = "Doe";
+		System.out.println(firstName.concat(lastName));
+		//Technical Details
+		//Returns:	An int value, representing the index of the first occurrence of the character in the string, or -1 if it never occurs
 		
 		
 		
